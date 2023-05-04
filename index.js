cc14ca192dcdf2a6430bbf16424da1f8b906019c
Justin Worley <jkworley@gmail.com>
2023-05-04 13:58:14 -0400

    completed lab

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const newTitles = tutorials.map((title) => {
+      const words = title.split(' ');
+      for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+      }
+      return words.join(' ');
+    })
+  return newTitles;
 }
+
+console.log(titleCased())
